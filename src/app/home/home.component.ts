78e2f3428509eb71d522db639622d69c994ef06f
prochini <you@example.com>
2022-09-21 17:07:31 +0800

    page content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,13 +13,16 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.slides[0] = {
-      src: './assets/img/angular.jpg',
+      src: './assets/img/caleb-russell-JmuyB_LibRo-unsplash.jpg',
     };
     this.slides[1] = {
-      src: './assets/img/react.jpg',
+      src: './assets/img/joey-csunyo-2EGuIR00UTk-unsplash.jpg',
     }
     this.slides[2] = {
-      src: './assets/img/vue.jpg',
+      src: './assets/img/photoholgic-1GFUOji-yck-unsplash.jpg',
+    }
+    this.slides[3] = {
+      src: './assets/img/victor-hM0jUUU5eDg-unsplash.jpg',
     }
   }
 
